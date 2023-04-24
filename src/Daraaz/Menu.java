373f77e92d039e7168e72e400d4216cab34d6804
deepu9848 <awasti.deepak2013@gmail.com>
2023-04-23 23:01:25 -0400

    data type

---
+++ src/Daraaz/Menu.java
@@ -0,0 +1,12 @@
+package Daraaz;
+
+public class Menu {
+    /*
+    long menuId(1);
+    String menuName womenFashion;
+    long parentMenuId (0);
+    boolean isLeaf (false);
+    String slug (women-fashion);
+
+     */
+}
