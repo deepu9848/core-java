6eff24c209793cb4702ddf646fd76133edd21613
deepu9848 <awasti.deepak2013@gmail.com>
2023-05-01 20:33:22 -0400

    Assignment

--- src/daraaz/Menu.java
+++ src/daraaz/Menu.java
@@ -1,19 +1,23 @@
 package daraaz;
 public class Menu {
-    public class menuId;
-    //UUID
+    public class  menuId;
+
+
+        //UUID
     /*
     Menu Id (long)
     Menu Name (String)
     Parent Menu Id (long)
     Is Leaf (boolean)
     Slug (String)
-     */long MenuID;
-     String MenuName;
-     long ParentMenuId;
-     boolean IsLeaf;
-     String Slug;
-}
+     */
+        long MenuID;
+        String MenuName;
+        long ParentMenuId;
+        boolean IsLeaf;
+        String Slug;
+    }
+
 
 
 
---
+++ src/day2/CalculationApp.java
@@ -0,0 +1,39 @@
+package day2;
+
+
+
+public class CalculationApp {
+    public  static void main(String[] args) {
+        CalculationApp calculationApp = new CalculationApp();
+
+        int a = 5;
+        int b = 7;
+        calculationApp.multiply(a, b);
+        float c = 3;
+        float d = 2;
+        calculationApp.multiply(c, d);
+        double e = 4;
+        double f = 1;
+        calculationApp.multiply(e, f);
+
+    }   public void multiplyInt(int a, int b){
+            int g = a * b;
+
+        public void MultiplyFloat(float c, float d);{
+            float h = c * d;
+        }
+
+        public void DoubleMultiplyDouble(double e, double f){
+            double i = e * f;
+
+    }
+
+
+
+
+
+
+
+
+
+
