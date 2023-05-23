417eb0a2e62cf5dc97f22507aa3fb951dd3a8ee1
deepu9848 <awasti.deepak2013@gmail.com>
2023-05-22 22:39:07 -0400

    looping

--- src/marksheet/ConditionalStatement.java
+++ src/marksheet/ConditionalStatement.java
@@ -1,5 +1,7 @@
 package marksheet;
 
+import java.util.Scanner;
+
 public class ConditionalStatement {
 
     public static void main(String[] args) {
@@ -11,7 +13,33 @@ public class ConditionalStatement {
         }
 
         System.out.println("5");
-
-
+        Scanner sc = new Scanner(System.in);
+        String weekDay = sc.next();
+        weekDay = weekDay. toLowerCase();
+        switch (weekDay) {
+            case "sunday":
+                System.out.println("salad");
+                break;
+            case "monday":
+                System.out.println("rice");
+                break;
+            case "tuesday":
+                System.out.println("corn");
+                break;
+            case "wednesday":
+                System.out.println("fruit");
+                break;
+            case "thursday":
+                System.out.println("noodles");
+                break;
+            case "friday":
+                System.out.println("pasta");
+                break;
+            case "saturday":
+                System.out.println("vegetable");
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ src/marksheet/Looping.java
@@ -0,0 +1,14 @@
+package marksheet;
+
+public class Looping {
+
+    public static void main(String[] args) {
+        //print numbers from 1to 100
+        int i =1;
+        while (i <=100) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
